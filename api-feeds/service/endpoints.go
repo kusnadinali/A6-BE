2eee41bc646c2fb024b50b0bdffa934ea92da3be
athirahnaurah <athirah.naurah.tif20@polban.ac.id>
2021-12-08 11:37:34 +0700

    add endpoints

--- api-feeds/service/endpoints.go
+++ api-feeds/service/endpoints.go
@@ -1,1 +1,63 @@
 package service
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"tes/datastruct"
+
+	"github.com/go-kit/kit/endpoint"
+)
+
+type (
+	// Endpoints define all endpoint
+	Endpoints struct {
+		GetFeeds endpoint.Endpoint
+	}
+
+	Response struct {
+		Status  bool               `json:"status"`
+		Message string             `json:"msg"`
+		Data    []datastruct.Feeds `json: "data"`
+	}
+
+	GetFeedsReq struct {
+		ID int `json:"id"`
+	}
+)
+
+func MakeFeedsEndpoints(svc Service) Endpoints {
+	return Endpoints{
+		GetFeeds: makeGetFeeds(svc),
+	}
+}
+
+func makeGetFeeds(svc Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		res, err := svc.GetFeeds(ctx)
+		if err != nil {
+			return Response{Status: false, Message: err.Error()}, nil
+		}
+		return Response{Status: true, Message: "succes", Data: res}, nil
+	}
+}
+
+// r teh apa isinya aih
+func decodeGetFeeds(_ context.Context, r *http.Request) (request interface{}, err error) {
+
+	return r, nil
+}
+
+func encodeResponse(ctx context.Context, w http.ResponseWriter, response interface{}) error {
+	res := response.(Response)
+
+	// ga pake package util, gatau buat apaan hshs
+
+	//sc := util.StatusCode(res.Message)
+	// if sc == 0 {
+	// 	sc = 500
+	// }
+	//w.WriteHeader(sc)
+
+	return json.NewEncoder(w).Encode(&res)
+}
