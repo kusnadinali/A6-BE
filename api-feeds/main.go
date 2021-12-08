e332a19d23920cdb46dc42b5ba6c0753784ef3be
HilmanWasiandiK <hilman.wasiandi.tif20@polban.ac.id>
2021-12-08 21:18:42 +0700

    add transport and main

--- api-feeds/main.go
+++ api-feeds/main.go
@@ -1,28 +1,35 @@
 package main
 
 import (
-	//	"context"
+	"context"
 	"database/sql"
 	"fmt"
+	"net/http"
+	"os"
+	"tes/service"
+	"tes/service/repository"
 
-	//	"net/http"
-	//	"os"
-	//	"tes/service"
-	//	"tes/service/repository"
-
-	//	"github.com/go-kit/kit/log"
-	//	"github.com/go-kit/kit/log/level"
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
 	_ "github.com/lib/pq"
 )
 
 var db *sql.DB
 
+const (
+	host       = "103.157.96.115"
+	port       = 5432
+	dbuser     = "shimano"
+	dbpassword = "milkyway"
+	dbname     = "db_shimano"
+)
+
 func initDB() {
-	connStr := "postgres://postgres:Kgdwfjrc123@localhost:5432/feeds?sslmode=disable"
+	dbSource := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, dbuser, dbpassword, dbname)
 
 	var err error
 
-	db, err = sql.Open("postgres", connStr)
+	db, err = sql.Open("postgres", dbSource)
 	if err != nil {
 		panic(err)
 	}
@@ -35,38 +42,38 @@ func initDB() {
 func main() {
 	initDB()
 
-	// var logger log.Logger
-	// {
-	// 	logger = log.NewLogfmtLogger(os.Stdout)
-	// 	logger = log.NewSyncLogger(logger)
-	// 	logger = log.With(logger,
-	// 		"service", "usermgmt",
-	// 		"time", log.DefaultTimestampUTC,
-	// 		"caller", log.DefaultCaller,
-	// 	)
-	// }
+	var logger log.Logger
+	{
+		logger = log.NewLogfmtLogger(os.Stdout)
+		logger = log.NewSyncLogger(logger)
+		logger = log.With(logger,
+			"service", "usermgmt",
+			"time", log.DefaultTimestampUTC,
+			"caller", log.DefaultCaller,
+		)
+	}
 
-	// var srv service.Service
-	// {
-	// 	svcRepository := repository.NewRepo(db, logger)
+	var srv service.Service
+	{
+		svcRepository := repository.NewRepo(db, logger)
 
-	// 	srv = service.NewService(
-	// 		svcRepository,
-	// 		// configs,
-	// 		logger,
-	// 	)
-	// }
-	// ctx := context.Background()
+		srv = service.NewService(
+			svcRepository,
+			// configs,
+			logger,
+		)
+	}
+	ctx := context.Background()
 
-	// endpoints := service.MakeFeedsEndpoints(srv)
+	endpoints := service.MakeFeedsEndpoints(srv)
 
-	// errChan := make(chan error)
+	errChan := make(chan error)
 
-	// go func() {
-	// 	handler := service.NewHTTPServer(ctx, endpoints)
-	// 	errChan <- http.ListenAndServe(":8080", handler)
+	go func() {
+		handler := service.NewHTTPServer(ctx, endpoints)
+		errChan <- http.ListenAndServe(":8080", handler)
 
-	// }()
+	}()
 
-	// level.Error(logger).Log("exit", <-errChan)
+	level.Error(logger).Log("exit", <-errChan)
 }
--- api-feeds/service/transport.go
+++ api-feeds/service/transport.go
@@ -1,1 +1,33 @@
 package service
+
+import (
+	"context"
+	"net/http"
+
+	httptransport "github.com/go-kit/kit/transport/http"
+	"github.com/gorilla/mux"
+)
+
+// NewHTTPServer...
+func NewHTTPServer(ctx context.Context, endpoints Endpoints) http.Handler {
+	r := mux.NewRouter()
+
+	r.Use(JSONHeader)
+
+	//tar tambahin parameter path pas udah nambahin function di database --> /postingan/{id}/{end}
+	r.Methods(http.MethodGet).Path("/postingan").Handler(httptransport.NewServer(
+		endpoints.GetFeeds,
+		decodeGetFeeds,
+		encodeResponse,
+	))
+
+	return r
+}
+
+// JSONHeader ...
+func JSONHeader(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
+	})
+}
